5c219e57eb58c370c63dacd5ed46bfc8d7cd384a
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-02 09:29:05 +0530

    string file

--- string.cpp
+++ string.cpp
@@ -1,1 +1,14 @@
-#include<
+#include<iostream>
+using namespace std;
+
+int main() {
+    string name;
+    string dept;
+    string grade;
+    cout<< "Enter your name,dept,grade:";
+    cin>> name >> dept >> grade;
+    cout<< "Name: " << name << endl;
+    cout<< "Department: " << dept << endl;
+    cout<< "Grade: " << grade << endl;
+    return 0;
+}
