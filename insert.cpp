46ae9d2c0e6e83b479b80a78bd622a93c373a26f
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-12 21:34:05 +0530

    inser

---
+++ insert.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(Node* &head, int val) {
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+
+void display(Node* head) {
+    while(head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+    Node* head = new Node(20);
+
+    insertAtHead(head, 10);
+
+    display(head);
+
+    return 0;
+}
