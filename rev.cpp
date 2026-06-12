8b755babacef214ddf55e37916d0aac318c8b584
Shreyas S <shreyaspoojari292@gmail.com>
2026-06-12 21:36:13 +0530

    rev

---
+++ rev.cpp
@@ -0,0 +1,17 @@
+Node* reverse(Node* head) {
+
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next;
+
+    while(curr != NULL) {
+
+        next = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
